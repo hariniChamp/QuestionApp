81b7b833ef4ae082d7d9f8aefeaeda4ac3f47f63
Harini <harinichamp02@gmail.com>
2024-06-07 12:57:18 -0500

    Initial Commit

---
+++ QuestionApp/QuestionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionAppApp.swift
+//  QuestionApp
+//
+//  Created by Scholar on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct QuestionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
